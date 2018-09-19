6aca10d7d768a7ad15a6489d0c4c4b418677dfbc
Aisha D'Souza <aishad.souza@yahoo.com>
2018-09-19 20:45:27 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,6 +3,7 @@ attr_accessor :name, :artist, :genre
 
 @@count = 0
 @@variable = []
+@@artist = 0
 
   def initialize(name, artist, genre)
     @name = name
@@ -10,6 +11,7 @@ attr_accessor :name, :artist, :genre
     @genre = genre
     @@count += 1
     @@variable << artist
+    @@artist += 1
   end
 
 
