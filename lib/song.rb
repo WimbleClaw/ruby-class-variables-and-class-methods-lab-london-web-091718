3f3ae70c361a5815867050c18eaa7edb854845dc
Aisha D'Souza <aishad.souza@yahoo.com>
2018-09-19 20:43:41 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,12 +2,14 @@ class Song
 attr_accessor :name, :artist, :genre
 
 @@count = 0
+@@variable = []
 
   def initialize(name, artist, genre)
     @name = name
     @artist =artist
     @genre = genre
     @@count += 1
+    @@variable += artist
   end
 
 
